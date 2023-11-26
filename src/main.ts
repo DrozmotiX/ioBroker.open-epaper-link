c9d0bd36a975ae2915c854fb3401a008e1031647
DutchmanNL <none@none.com>
2023-11-26 21:27:13 +0100

    add ip validator

--- src/main.ts
+++ src/main.ts
@@ -262,6 +262,10 @@ class OpenEpaperLink extends utils.Adapter {
 		}
 	}
 
+	private validateIPAddress(ipAddress: string): boolean {
+		return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+			ipAddress,
+		);
 	}
 }
 
