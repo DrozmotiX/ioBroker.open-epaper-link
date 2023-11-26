e9797e8d7eec78167bbaad971172a73ccc669782
DutchmanNL <none@none.com>
2023-11-26 21:33:21 +0100

    add webSocket Handler

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,22 @@
  * Created with @iobroker/create-adapter v2.5.0
  */
 
+type ApConnection = {
+	[key: string]: {
+		ip: string;
+		connection: WebSocket;
+		connectionStatus: string;
+		deviceName: string;
+	};
+};
+
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 import * as utils from '@iobroker/adapter-core';
+import WebSocket from 'ws';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+const apConnection: ApConnection = [];
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
@@ -23,7 +36,6 @@ class OpenEpaperLink extends utils.Adapter {
 		this.on('ready', this.onReady.bind(this));
 		this.on('stateChange', this.onStateChange.bind(this));
 		this.on('message', this.onMessage.bind(this));
-		// this.on('message', this.onMessage.bind(this));
 		this.on('unload', this.onUnload.bind(this));
 		jsonExplorer.init(this, stateAttrb); // Initiate library to handle JSOn data & state creation
 	}
@@ -37,55 +49,55 @@ class OpenEpaperLink extends utils.Adapter {
 		// Reset the connection indicator during startup
 		this.setState('info.connection', false, true);
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info('config option1: ' + this.config.option1);
-		this.log.info('config option2: ' + this.config.option2);
+		// Connect to test-device
+		this.wsConnectionHandler('192.168.10.150');
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync('testVariable', {
-			type: 'state',
-			common: {
-				name: 'testVariable',
-				type: 'boolean',
-				role: 'indicator',
-				read: true,
-				write: true,
-			},
-			native: {},
+		// ToDo: Establish connection to all already known devices
+		this.setState('info.connection', true, true);
+	}
+
+	private wsConnectionHandler(deviceIP: string): void {
+		apConnection[deviceIP] = {
+			connection: new WebSocket(`ws://${deviceIP}/ws`),
+			connectionStatus: 'Connecting',
+			deviceName: 'testDevice',
+			ip: deviceIP,
+		};
+
+		apConnection[deviceIP].connection.on('open', () => {
+			this.log.info('Connected to server');
+			apConnection[deviceIP].connectionStatus = 'Connected';
+			//ToDo: Create Device on connection state and store decide details
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			this.extendObject(apConnection[deviceIP].deviceName, {
+				type: 'device',
+				common: {
+					name: apConnection[deviceIP].deviceName,
+					// ToDo: @ticaki please assit with TS, value is correct but error shown
+					// statusStates: {
+					// 	onlineId: `${this.namespace}.${apConnection[deviceIP].deviceName}._info._online`,
+					// },
+				},
+				native: {
+					ip: apConnection[deviceIP].ip,
+				},
+			});
 		});
 
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates('testVariable');
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates('lights.*');
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates('*');
+		apConnection[deviceIP].connection.on('message', (message: string) => {
+			//ToDo: Design messageHandler to write values to states
+			this.log.info(`Received message from server: ${message}`);
+			message = JSON.parse(message);
 
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync('testVariable', true);
+			//ToDo: modify JSON data to store states into correct channel
+			jsonExplorer.traverseJson(message, apConnection[deviceIP].deviceName);
+			apConnection[deviceIP].connectionStatus = 'Connected';
+		});
 
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync('testVariable', { val: true, ack: true });
-
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
-
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync('admin', 'iobroker');
-		this.log.info('check user admin pw iobroker: ' + result);
-
-		result = await this.checkGroupAsync('admin', 'admin');
-		this.log.info('check group user admin group admin: ' + result);
+		apConnection[deviceIP].connection.on('close', () => {
+			this.log.info('Disconnected from server');
+			apConnection[deviceIP].connectionStatus = 'Disconnected';
+		});
 	}
 
 	/**
@@ -99,26 +111,21 @@ class OpenEpaperLink extends utils.Adapter {
 			// ...
 			// clearInterval(interval1);
 
+			// loop truth all connection and close if present
+			for (const ap in apConnection) {
+				//ToDo: needs to be optimized, just quick & dirty for testing now
+				try {
+					apConnection[ap].connection.close();
+				} catch (e) {
+					// no connection present
+				}
+			}
+
 			callback();
 		} catch (e) {
 			callback();
 		}
 	}
-
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  */
-	// private onObjectChange(id: string, obj: ioBroker.Object | null | undefined): void {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
 
 	/**
 	 * Is called if a subscribed state changes
